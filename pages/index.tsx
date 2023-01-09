b31e08d2a1ceecad90736354f9233cfb991b6f83
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-09 20:31:09 +0900

    NDHR-10 연동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Layout from "../components/layout";
+
 export default function Home() {
-  return <div>Hello, World!</div>;
+  return (
+    <Layout>
+      <div>Hello, World!</div>
+    </Layout>
+  );
 }
