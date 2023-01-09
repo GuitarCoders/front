a887a204fb8a22aea11ba21627e9adb471691b37
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-09 17:09:27 +0900

    minimal layout setup

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,12 @@
-const Layout = () => {
-  return null;
+import { NextPage } from "next";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode | ReactNode[];
+}
+
+const Layout: NextPage<LayoutProps> = ({ children }) => {
+  return <main className="max-w-2xl mx-auto">{children}</main>;
 };
 
 export default Layout;
