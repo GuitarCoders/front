3fe6e04ed1b8b89888cfe717a5bf5e97a98b121c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-15 16:56:07 +0900

    NDHR-42 user 라우트 생성

---
+++ pages/user.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Layout from "../components/layout";
+
+const User: NextPage = () => {
+  return (
+    <Layout title="나는">
+      <section></section>
+    </Layout>
+  );
+};
+
+export default User;
