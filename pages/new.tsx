05a17c9b186e0fbb3db2079efaf2a27ee0056f0c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-24 00:11:05 +0900

    NDHR-56 새 글 작성 페이지 작성

---
+++ pages/new.tsx
@@ -0,0 +1,32 @@
+import Layout from "../components/layout";
+import { PlusIcon } from "../styles/icons";
+
+const New = () => {
+  return (
+    <Layout hideLayouts>
+      <section className="p-4">
+        <form className="flex flex-col gap-3">
+          <textarea
+            className="border w-full resize-none rounded-md p-4 focus:ring-violet-400 focus:outline-violet-400"
+            placeholder="무슨 일이 일어나고 있나요?"
+            rows={3}
+          />
+          <div>
+            <label
+              htmlFor="media"
+              className="w-14 h-14 border-2 border-dashed rounded-md text-gray-300 flex justify-center items-center hover:text-gray-700 hover:border-gray-700 hover:cursor-pointer"
+            >
+              <PlusIcon className="w-8 h-8" />
+            </label>
+            <input type="file" className="hidden" id="media" />
+          </div>
+          <button className="px-4 py-2 bg-violet-400 text-white rounded-md">
+            글쓰기
+          </button>
+        </form>
+      </section>
+    </Layout>
+  );
+};
+
+export default New;
