050386f8a2c8a27d1b9fe2b9919887a276d88a36
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-23 23:43:13 +0900

    NDHR-42 프로필 우상단 버튼 추가

--- pages/user.tsx
+++ pages/user.tsx
@@ -7,8 +7,19 @@ const User: NextPage = () => {
   return (
     <Layout title="나는">
       <section>
-        <div className="h-56 bg-slate-100 flex items-end p-4">
+        <div className="h-56 bg-slate-100 flex flex-col justify-end p-4 gap-3 relative">
+          <div className="absolute right-4 top-4 flex gap-2">
+            <button className="border border-violet-400 bg-white px-2 py-1 text-sm rounded-md text-violet-400">
+              친구 82
+            </button>
+            <button className="border border-violet-400 bg-white px-2 py-1 text-sm rounded-md text-violet-400">
+              구독 12
+            </button>
+          </div>
           <Profile />
+          <p className="text-xs text-slate-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </p>
         </div>
         <div className="grid grid-cols-4 h-12 border-b">
           <button className="text-violet-400 font-bold">이야기</button>
