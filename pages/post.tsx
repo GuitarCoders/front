883359dc28fa077da8ce583a6c591f93a7ecd739
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-23 19:14:17 +0900

    NDHR-39 새 글 작성 빈 파일 추가

---
+++ pages/post.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+import PostForm from "../components/post-form";
+
+const Post = () => {
+  return (
+    <Layout>
+      <PostForm />
+    </Layout>
+  );
+};
+
+export default Post;
