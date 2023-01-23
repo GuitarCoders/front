203372fc9b7e22ec57e8e394d47f44186dc9e030
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-24 00:10:58 +0900

    NDHR-56 레이아웃 숨기기 옵션 추가

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,30 +6,36 @@ import {
   UserIcon,
   UsersIcon,
 } from "../styles/icons";
+import { cls } from "../utils/cls";
 import FooterItem from "./footer-item";
 
 interface LayoutProps {
   title?: string;
+  hideLayouts?: true;
   children: ReactNode | ReactNode[];
 }
 
-const Layout: NextPage<LayoutProps> = ({ title, children }) => {
+const Layout: NextPage<LayoutProps> = ({ title, hideLayouts, children }) => {
   return (
     <main className="max-w-2xl mx-auto">
-      <header className="h-16 border-b text-center shadow-sm flex justify-center items-center fixed top-0 max-w-2xl w-full bg-white z-10">
-        <h1 className="text-lg font-bold">{title}</h1>
-      </header>
-      <section className="mt-16 mb-20">{children}</section>
-      <footer className="max-w-2xl w-full grid grid-cols-4 mx-auto h-20 bg-white fixed bottom-0 border-t">
-        <FooterItem
-          title="모아보는"
-          link="timeline"
-          icon={<RectangleStackIcon />}
-        />
-        <FooterItem title="멘션" link="mention" icon={<AtSymbolIcon />} />
-        <FooterItem title="나는" link="user" icon={<UserIcon />} />
-        <FooterItem title="친구들은" link="friends" icon={<UsersIcon />} />
-      </footer>
+      {hideLayouts ? null : (
+        <header className="h-16 border-b text-center shadow-sm flex justify-center items-center fixed top-0 max-w-2xl w-full bg-white z-10">
+          <h1 className="text-lg font-bold">{title}</h1>
+        </header>
+      )}
+      <section className={hideLayouts ? "" : "mt-16 mb-20"}>{children}</section>
+      {hideLayouts ? null : (
+        <footer className="max-w-2xl w-full grid grid-cols-4 mx-auto h-20 bg-white fixed bottom-0 border-t">
+          <FooterItem
+            title="모아보는"
+            link="timeline"
+            icon={<RectangleStackIcon />}
+          />
+          <FooterItem title="멘션" link="mention" icon={<AtSymbolIcon />} />
+          <FooterItem title="나는" link="user" icon={<UserIcon />} />
+          <FooterItem title="친구들은" link="friends" icon={<UsersIcon />} />
+        </footer>
+      )}
     </main>
   );
 };
