3f472b190682b58acb9e970a6851bba299a48538
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-23 23:30:46 +0900

    NDHR-42 유저 페이지 외관 작성

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,10 +1,27 @@
 import { NextPage } from "next";
 import Layout from "../components/layout";
+import PostPreview from "../components/post-preview";
+import Profile from "../components/profile";
 
 const User: NextPage = () => {
   return (
     <Layout title="나는">
-      <section></section>
+      <section>
+        <div className="h-56 bg-slate-100 flex items-end p-4">
+          <Profile />
+        </div>
+        <div className="grid grid-cols-4 h-12 border-b">
+          <button className="text-violet-400 font-bold">이야기</button>
+          {["미디어", "글감", "미투함"].map((i) => (
+            <button key={i}>{i}</button>
+          ))}
+        </div>
+        <div className="divide-y">
+          {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+            <PostPreview key={i} />
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 };
