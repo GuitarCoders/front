d2613d85bafbb2945145d1b6b3c69519ed1b77b6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-24 00:10:41 +0900

    NDHR-56 fib에 onclick prop 추가

--- components/fib.tsx
+++ components/fib.tsx
@@ -2,11 +2,15 @@ import { ReactNode } from "react";
 
 interface FIBProps {
   icon: ReactNode;
+  onClick: () => void;
 }
 
-const FIB = ({ icon }: FIBProps) => {
+const FIB = ({ icon, onClick }: FIBProps) => {
   return (
-    <button className="flex justify-center items-center fixed w-16 h-16 bg-violet-300 rounded-full bottom-24 right-4 text-white shadow-lg">
+    <button
+      onClick={onClick}
+      className="flex justify-center items-center fixed w-16 h-16 bg-violet-300 rounded-full bottom-24 right-4 text-white shadow-lg"
+    >
       {icon}
     </button>
   );
