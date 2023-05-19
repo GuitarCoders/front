77b48d2cdedc529c0bedd0763510120b9e1ff052
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-19 23:29:44 +0900

    빈 페이지 생성

---
+++ pages/chat.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+const Mention = () => {
+  return (
+    <Layout title="메시지">
+      <></>
+    </Layout>
+  );
+};
+
+export default Mention;
---
+++ pages/friends.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+const Friends = () => {
+  return (
+    <Layout title="친구들은">
+      <h1>친구들은</h1>
+    </Layout>
+  );
+};
+
+export default Friends;
