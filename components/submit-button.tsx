942e29dcfb6f22c617c33b37fa02bc21463502d6
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-20 18:31:13 +0900

    loading prop 추가

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -2,12 +2,13 @@ import { NextPage } from "next";
 
 interface SubmitButtonProps {
   text: string;
+  loading?: boolean;
 }
 
-const SubmitButton: NextPage<SubmitButtonProps> = ({ text }) => {
+const SubmitButton: NextPage<SubmitButtonProps> = ({ text, loading }) => {
   return (
     <button className="px-4 py-2 bg-violet-400 text-white rounded-md outline-none focus:ring-2 focus:ring-violet-600">
-      {text}
+      {loading ? "로딩 중..." : text}
     </button>
   );
 };
