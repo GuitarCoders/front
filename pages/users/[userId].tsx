abb0cb606a6c2d0578a91ef99846f70daf301a03
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-20 14:42:37 +0900

    유저 페이지 정리

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/layout";
+import UserTemplate from "@components/user-template";
+
+const User = () => {
+  return (
+    <Layout canGoBack>
+      <UserTemplate />
+    </Layout>
+  );
+};
+
+export default User;
