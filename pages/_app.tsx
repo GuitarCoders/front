1e0a2dcba39f28e484061aea8b0b406ba8c1b400
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-20 18:42:05 +0900

    apollo/client 사용 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.BASE_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
