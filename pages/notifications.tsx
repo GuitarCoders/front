55b90b8e1812e7b3214807dd79235cd4df365d98
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-20 14:47:00 +0900

    Create notifications.tsx

---
+++ pages/notifications.tsx
@@ -0,0 +1,11 @@
+import Layout from "@components/layout";
+
+const Notifications = () => {
+  return (
+    <Layout title="알림" canGoBack>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default Notifications;
