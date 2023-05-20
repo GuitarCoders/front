111fac8b7da8e1dc48b6931918295bc9411b9c90
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-20 14:08:53 +0900

    새 챗 더미 페이지 작성

---
+++ pages/chat/new.tsx
@@ -0,0 +1,11 @@
+import Layout from "@components/layout";
+
+const NewChat = () => {
+  return (
+    <Layout canGoBack>
+      <div>CREATE NEW CHAT HERE!</div>
+    </Layout>
+  );
+};
+
+export default NewChat;
