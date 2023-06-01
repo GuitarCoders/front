8fe0f1f592ad332124ac82b53b34fea469b82a69
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-01 21:57:13 +0900

    일반 버튼 역할도 수행할 수 있도록 변경

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -1,13 +1,32 @@
+import { cls } from "@libs/cls";
 import { NextPage } from "next";
 
 interface SubmitButtonProps {
   text: string;
   loading?: boolean;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+  destructive?: boolean;
 }
 
-const SubmitButton: NextPage<SubmitButtonProps> = ({ text, loading }) => {
+const SubmitButton: NextPage<SubmitButtonProps> = ({
+  text,
+  loading,
+  type = "submit",
+  onClick,
+  destructive = false,
+}) => {
   return (
-    <button className="px-4 py-2 bg-violet-400 text-white rounded-md outline-none focus:ring-2 focus:ring-violet-600">
+    <button
+      type={type}
+      onClick={onClick}
+      className={cls(
+        "px-4 py-2 text-white rounded-md outline-none focus:ring-2 ",
+        destructive
+          ? "bg-rose-600 focus:ring-rose-700"
+          : "bg-violet-400 focus:ring-violet-600"
+      )}
+    >
       {loading ? "로딩 중..." : text}
     </button>
   );
