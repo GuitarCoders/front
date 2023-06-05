9076ed3983a29989b257639e1867696e2f04afbe
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-05 20:19:55 +0900

    apollo client를 SSR에서도 활용할 수 있도록 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,16 @@
 import "@styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloProvider } from "@apollo/client";
 import AlertDialogComponent from "@components/alert-dialog";
 import { RecoilRoot } from "recoil";
-
-const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_BASE_URL,
-});
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { useApollo } from "@libs/apollo-client";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <RecoilRoot>
         <AlertDialogComponent />
         <Component {...pageProps} />
