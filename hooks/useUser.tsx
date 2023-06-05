5c70ae0139a7b70b9e2e5285dca8da97f9ebbba0
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-05 17:06:15 +0900

    _id 대신 accountId로 검색
    주어진 accountId로도 검색할 수 있도록 옵션 추가

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,9 +1,10 @@
 import { ApolloError, gql, useLazyQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import useAlert from "./useAlert";
 
-const USER_BY_ID = gql`
-  query UserById($id: String!) {
-    userById(id: $id) {
+const USER_BY_ACCOUNT_ID = gql`
+  query UserByAccountId($account_id: String!) {
+    userByAccountId(account_id: $account_id) {
       _id
       name
       email
@@ -21,32 +22,49 @@ export interface User {
   about_me: string;
   friends: string[];
 }
-interface UserByIdResponse {
-  userById: {
+interface UserByAccountIdResponse {
+  userByAccountId: {
     __typename: "User";
   } & User;
 }
 export type UserState = User | null | undefined;
 type UseUserReturn = [UserState, { loading: boolean; error?: ApolloError }];
 
-export default function useUser(): UseUserReturn {
+export default function useUser(requestedAccountId?: string): UseUserReturn {
   const [user, setUser] = useState<UserState>(null);
-  const [userById, { loading, error }] = useLazyQuery<
-    UserByIdResponse,
-    { id: string }
-  >(USER_BY_ID);
+  const alert = useAlert();
+  const [userByAccountId, { loading, error }] = useLazyQuery<
+    UserByAccountIdResponse,
+    { account_id: string }
+  >(USER_BY_ACCOUNT_ID);
+
+  const getUser = useCallback(
+    async (account_id: string) => {
+      const result = await userByAccountId({
+        variables: { account_id },
+      });
+      return result.data?.userByAccountId;
+    },
+    [userByAccountId]
+  );
 
   useEffect(() => {
-    const userId = localStorage.getItem("_id");
-    if (userId === null) {
+    const userAccountId = localStorage.getItem("account_id");
+    if (userAccountId === null) {
+      alert({
+        visible: true,
+        title: "로그인 알림",
+        description: "서비스를 이용하려면 로그인이 필요해요.",
+        relogin: true,
+      });
       setUser(undefined);
     } else {
       (async () => {
-        const result = await userById({ variables: { id: userId } });
-        setUser(result.data?.userById);
+        const user = await getUser(requestedAccountId ?? userAccountId);
+        setUser(user);
       })();
     }
-  }, [userById]);
+  }, [alert, getUser, requestedAccountId]);
 
   return [user, { loading, error }];
 }
