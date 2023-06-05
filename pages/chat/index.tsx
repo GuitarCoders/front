9fb0a9c1c0c6e8a6389f11dfb823c22c23c57e80
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-06 01:33:35 +0900

    serverSideProps 구문 추가

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,31 +1,42 @@
 import Layout from "@components/layout";
 import useUser from "hooks/useUser";
+import { GetServerSidePropsContext, NextPage } from "next";
+import cookies from "next-cookies";
 import Link from "next/link";
 
-const Mention = () => {
-  const [user] = useUser();
+interface ChatProps {
+  accountId?: string;
+}
+
+const Chat: NextPage<ChatProps> = ({ accountId }) => {
+  if (!accountId) return null;
   return (
     <Layout title="메시지" showNewChatBtn>
-      {!user ? null : (
-        <section>
-          {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-            <Link href={`/chat/${i}`} key={i}>
-              <div className="flex items-center border-b p-4 gap-4">
-                <div className="w-12 h-12 bg-gray-300 rounded-md shrink-0" />
-                <div className="flex flex-col gap-1 text-sm">
-                  <h5 className="font-semibold">닉네임</h5>
-                  <p className="font-light text-xs">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Placeat, quasi!
-                  </p>
-                </div>
+      <section>
+        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+          <Link href={`/chat/${i}`} key={i}>
+            <div className="flex items-center border-b p-4 gap-4">
+              <div className="w-12 h-12 bg-gray-300 rounded-md shrink-0" />
+              <div className="flex flex-col gap-1 text-sm">
+                <h5 className="font-semibold">닉네임</h5>
+                <p className="font-light text-xs">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Placeat, quasi!
+                </p>
               </div>
-            </Link>
-          ))}
-        </section>
-      )}
+            </div>
+          </Link>
+        ))}
+      </section>
     </Layout>
   );
 };
 
-export default Mention;
+export function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const { accountId } = cookies(ctx);
+  return {
+    props: { accountId },
+  };
+}
+
+export default Chat;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,31 @@ import { useRouter } from "next/router";
 import Layout from "@components/layout";
 import PostPreview from "@components/post-preview";
 import useUser from "hooks/useUser";
+import { GetServerSidePropsContext } from "next";
+import cookies from "next-cookies";
 
-export default function Timeline() {
-  const [user] = useUser();
+interface TimelineProps {
+  accountId?: string;
+}
+
+export default function Timeline({ accountId }: TimelineProps) {
   const { push } = useRouter();
   const pushToNewPage = () => push("/new");
 
   return (
     <Layout title="모아보는" showNewPostBtn>
-      {!user ? null : (
-        <section className="divide-y">
-          {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-            <PostPreview key={i} />
-          ))}
-        </section>
-      )}
+      <section className="divide-y">
+        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+          <PostPreview key={i} />
+        ))}
+      </section>
     </Layout>
   );
+}
+
+export function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const { accountId } = cookies(ctx);
+  return {
+    props: { accountId },
+  };
 }
