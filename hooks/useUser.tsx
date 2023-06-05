8e4fae616e8a9d0601f5364da6e4e195d3fc832e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-05 14:04:24 +0900

    NDHR-79 유저 데이터를 서버 응답으로 변경

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,23 +1,52 @@
+import { ApolloError, gql, useLazyQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 
+const USER_BY_ID = gql`
+  query UserById($id: String!) {
+    userById(id: $id) {
+      _id
+      name
+      email
+      account_id
+      about_me
+      friends
+    }
+  }
+`;
 export interface User {
   _id: string;
   name: string;
   email: string;
   account_id: string;
   about_me: string;
+  friends: string[];
 }
-type UseUserResult = User | null | undefined;
+interface UserByIdResponse {
+  userById: {
+    __typename: "User";
+  } & User;
+}
+export type UserState = User | null | undefined;
+type UseUserReturn = [UserState, { loading: boolean; error?: ApolloError }];
 
-export default function useUser(): UseUserResult {
-  const [user, setUser] = useState<UseUserResult>(null);
+export default function useUser(): UseUserReturn {
+  const [user, setUser] = useState<UserState>(null);
+  const [userById, { loading, error }] = useLazyQuery<
+    UserByIdResponse,
+    { id: string }
+  >(USER_BY_ID);
+
   useEffect(() => {
-    const lsUser = localStorage.getItem("user");
-    if (lsUser === null) {
+    const userId = localStorage.getItem("_id");
+    if (userId === null) {
       setUser(undefined);
     } else {
-      setUser(JSON.parse(lsUser));
+      (async () => {
+        const result = await userById({ variables: { id: userId } });
+        setUser(result.data?.userById);
+      })();
     }
-  }, []);
-  return user;
+  }, [userById]);
+
+  return [user, { loading, error }];
 }
