67a411e3e7f292fb4d36448cbf40ba3e83a053d3
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-05 17:06:46 +0900

    유저 인증과정 추가

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,25 +1,29 @@
 import Layout from "@components/layout";
+import useUser from "hooks/useUser";
 import Link from "next/link";
 
 const Mention = () => {
+  const [user] = useUser();
   return (
     <Layout title="메시지" showNewChatBtn>
-      <section>
-        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-          <Link href={`/chat/${i}`} key={i}>
-            <div className="flex items-center border-b p-4 gap-4">
-              <div className="w-12 h-12 bg-gray-300 rounded-md shrink-0" />
-              <div className="flex flex-col gap-1 text-sm">
-                <h5 className="font-semibold">닉네임</h5>
-                <p className="font-light text-xs">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Placeat, quasi!
-                </p>
+      {!user ? null : (
+        <section>
+          {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+            <Link href={`/chat/${i}`} key={i}>
+              <div className="flex items-center border-b p-4 gap-4">
+                <div className="w-12 h-12 bg-gray-300 rounded-md shrink-0" />
+                <div className="flex flex-col gap-1 text-sm">
+                  <h5 className="font-semibold">닉네임</h5>
+                  <p className="font-light text-xs">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Placeat, quasi!
+                  </p>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </section>
+            </Link>
+          ))}
+        </section>
+      )}
     </Layout>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import { useRouter } from "next/router";
 import Layout from "@components/layout";
 import PostPreview from "@components/post-preview";
+import useUser from "hooks/useUser";
 
 export default function Timeline() {
+  const [user] = useUser();
   const { push } = useRouter();
   const pushToNewPage = () => push("/new");
 
   return (
     <Layout title="모아보는" showNewPostBtn>
-      <section className="divide-y">
-        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-          <PostPreview key={i} />
-        ))}
-      </section>
+      {!user ? null : (
+        <section className="divide-y">
+          {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+            <PostPreview key={i} />
+          ))}
+        </section>
+      )}
     </Layout>
   );
 }
