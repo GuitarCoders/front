565f02125e9ba89a39ed04d1132e293f388f5eed
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-07 11:55:40 +0900

    NDHR-78 다른 유저 정보 페이지 작성

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -6,13 +6,23 @@ import { UserByAccountIdResponse } from "graphql/quries.type";
 import { GetServerSidePropsContext, NextPage } from "next";
 import cookies from "next-cookies";
 import type { User } from "hooks/useUser";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 interface UserPageProps {
-  profile: User;
+  profile: User | null;
   isMe: boolean;
 }
 
 const UserPage: NextPage<UserPageProps> = ({ isMe, profile }) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!profile) {
+      router.push("/404");
+    }
+  }, [profile, router]);
+
+  if (!profile) return null;
   return (
     <Layout canGoBack title={profile.name}>
       <UserTemplate profile={profile} isMe={isMe} />
@@ -24,19 +34,23 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const { accountId, accessToken } = cookies(ctx);
   const userId = ctx.params?.userId;
   const isMe = accountId === userId;
-
   const apolloClient = initializeApollo(null, accessToken);
 
-  const {
-    data: { userByAccountId },
-  } = await apolloClient.query<UserByAccountIdResponse>({
-    query: USER_BY_ACCOUNT_ID,
-    variables: { account_id: userId },
-  });
-
-  return {
-    props: { profile: userByAccountId, isMe },
-  };
+  try {
+    const {
+      data: { userByAccountId },
+    } = await apolloClient.query<UserByAccountIdResponse>({
+      query: USER_BY_ACCOUNT_ID,
+      variables: { account_id: userId },
+    });
+    return {
+      props: { profile: userByAccountId, isMe },
+    };
+  } catch {
+    return {
+      props: { profile: null },
+    };
+  }
 }
 
 export default UserPage;
