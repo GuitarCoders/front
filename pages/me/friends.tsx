d0256c70a9fc17c5e4a1e683a79e63aec150f23f
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-09 17:47:29 +0900

    친구 신청 로딩 중 스켈레톤 UI 표시

--- pages/me/friends.tsx
+++ pages/me/friends.tsx
@@ -1,33 +1,50 @@
 import Layout from "@components/layout";
+import { initializeApollo } from "@libs/apollo-client";
+import { USER_BY_ACCOUNT_ID } from "graphql/quries";
+import { UserByAccountIdResponse } from "graphql/quries.type";
+import { GetServerSidePropsContext, NextPage } from "next";
+import cookies from "next-cookies";
 import Link from "next/link";
 
-const MyFriends = () => {
+const friendRequestRoutes = [
+  { link: "/me/sent", title: "보낸 친구 신청" },
+  { link: "/me/received", title: "받은 친구 신청" },
+];
+
+interface MyFriendsProps {
+  friends: string[];
+}
+
+const MyFriends: NextPage<MyFriendsProps> = ({ friends }) => {
+  console.log(friends);
   return (
     <Layout canGoBack title="친구">
-      <section className="p-4">
-        <Link href="/me/requested">
-          <div className="rounded-lg shadow-lg">
-            <div className="p-4 flex justify-between">
-              <h1 className="">받은 친구 신청</h1>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                />
-              </svg>
+      <section className="p-4 mb-2 flex flex-col gap-4">
+        {friendRequestRoutes.map((route) => (
+          <Link href={route.link} key={route.link}>
+            <div className="p-4 rounded-lg border shadow-sm">
+              <div className="flex justify-between">
+                <h1>{route.title}</h1>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                  />
+                </svg>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </section>
-      <section className="divide-y">
+      <section className="divide-y pb-6">
         {Array.from({ length: 10 }, (_, i) => i).map((i) => (
           <Link href="/users/tester" key={i} className="flex gap-4 px-4 py-2">
             <div className="w-14 h-14 bg-gray-200 rounded-md shrink-0" />
@@ -41,5 +58,29 @@ const MyFriends = () => {
     </Layout>
   );
 };
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const { accountId, accessToken } = cookies(ctx);
+  const apolloClient = initializeApollo(null, accessToken);
+
+  try {
+    const {
+      data: {
+        userByAccountId: { friends },
+      },
+    } = await apolloClient.query<UserByAccountIdResponse>({
+      query: USER_BY_ACCOUNT_ID,
+      variables: { account_id: accountId },
+    });
+
+    return {
+      props: { friends },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+}
 
 export default MyFriends;
