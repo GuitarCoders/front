43bd781eb3eefd224fea502316747fb6f0a4a09b
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-29 00:43:03 +0900

    NDHR-98 친구가 아닌 유저의 타임라인 API 연결

--- pages/users/[userAccountId]/index.tsx
+++ pages/users/[userAccountId]/index.tsx
@@ -8,6 +8,51 @@ import cookies from "next-cookies";
 import type { User } from "hooks/useUser";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query GetPosts($count: Int!, $filter: filter) {
+    getPosts(getPostsData: { count: $count, filter: $filter }) {
+      posts {
+        _id
+        author {
+          _id
+          name
+        }
+        content
+        tags
+        category
+        createdAt
+      }
+      lastDateTime
+    }
+  }
+`;
+
+interface Filter {
+  userId?: string;
+  category?: string;
+  before?: string;
+}
+
+interface GetPostsForm {
+  count: number;
+  filter?: Filter;
+}
+
+interface GetPostsResponse {
+  getPosts: {
+    posts: {
+      _id: string;
+      author: User;
+      content: string;
+      tags: string;
+      category: string;
+      createdAt: string;
+    }[];
+    lastDateTime: string;
+  };
+}
 
 interface UserPageProps {
   profile: User | null;
@@ -22,10 +67,22 @@ const UserPage: NextPage<UserPageProps> = ({ isMe, profile }) => {
     }
   }, [profile, router]);
 
+  const { data, loading, refetch } = useQuery<GetPostsResponse, GetPostsForm>(
+    GET_POSTS,
+    {
+      variables: { count: 20, filter: { userId: profile?._id } },
+    }
+  );
+
   if (!profile) return null;
   return (
     <Layout canGoBack title={profile.name}>
-      <UserTemplate profile={profile} isMe={isMe} />
+      <UserTemplate
+        profile={profile}
+        isMe={isMe}
+        data={data}
+        dataLoading={loading}
+      />
     </Layout>
   );
 };
