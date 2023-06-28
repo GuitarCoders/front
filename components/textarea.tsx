62d9484598eb60669ddebd94a0af9e27af1f838a
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-28 17:13:39 +0900

    인풋과 스타일 통일

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -14,7 +14,7 @@ const Textarea: NextPage<TextareaProps> = ({
 }) => {
   return (
     <textarea
-      className="border w-full resize-none border-gray-300 shadow-sm rounded-md px-4 py-2 focus:ring-2 focus:ring-violet-600 outline-none"
+      className="border w-full resize-none border-gray-300 shadow-sm rounded-md px-4 py-2 focus:ring-2 focus:ring-violet-600 focus:border-violet-600 outline-none"
       {...register}
       placeholder={placeholder}
       defaultValue={defaultValue}
