6a34389b8d8fbae716f68c6eed22021be35b2181
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-07-26 17:05:50 +0900

    form과 연결하도록 수정

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,22 +1,25 @@
 import { NextPage } from "next";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface ChatInputProps {
   placeholder?: string;
+  register: UseFormRegisterReturn;
 }
 
-const ChatInput: NextPage<ChatInputProps> = ({ placeholder }) => {
+const ChatInput: NextPage<ChatInputProps> = ({ placeholder, register }) => {
   return (
     <div className="fixed bottom-0 p-4 w-full max-w-2xl mx-auto">
-      <form className="relative">
+      <div className="relative">
         <input
           type="text"
           className="w-full border pl-4 pr-14 py-2 rounded-3xl focus:ring-2 ring-violet-400 focus:outline-none"
           placeholder={placeholder}
+          {...register}
         />
         <button className="absolute right-2 top-[5px] text-md py-1 px-4 bg-violet-400 rounded-3xl text-white">
           →
         </button>
-      </form>
+      </div>
     </div>
   );
 };
