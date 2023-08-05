56cdb7c9cbd26aa16112209059b7182bc3048125
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-08-05 23:51:23 +0900

    색상 연하게 수정

--- components/skeletons/sk-comment.tsx
+++ components/skeletons/sk-comment.tsx
@@ -1,10 +1,10 @@
 const SkComment = () => {
   return (
     <div className="p-4 flex gap-4 align-start border-b">
-      <div className="w-10 h-10 rounded-md bg-gray-300 flex-shrink-0 animate-pulse" />
+      <div className="w-10 h-10 rounded-md bg-gray-200 flex-shrink-0 animate-pulse" />
       <div className="flex flex-col text-sm gap-1 justify-center w-full">
-        <div className="w-8 h-4 bg-gray-300 rounded-md animate-pulse" />
-        <div className="w-44 h-4 bg-gray-300 rounded-md animate-pulse" />
+        <div className="w-8 h-4 bg-gray-200 rounded-md animate-pulse" />
+        <div className="w-44 h-4 bg-gray-200 rounded-md animate-pulse" />
       </div>
     </div>
   );
