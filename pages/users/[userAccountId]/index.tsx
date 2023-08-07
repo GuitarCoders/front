b291a13da3f842d5035f41476fdc971dcfb6a38b
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-08-07 16:57:19 +0900

    /users 페이지에도 targetUserId 연결

--- pages/users/[userAccountId]/index.tsx
+++ pages/users/[userAccountId]/index.tsx
@@ -1,58 +1,18 @@
 import Layout from "@components/layout";
 import UserTemplate from "@components/user-template";
 import { addApolloState, initializeApollo } from "@libs/apollo-client";
-import { USER_BY_ACCOUNT_ID } from "graphql/quries";
-import { UserByAccountIdResponse } from "graphql/quries.type";
+import { GET_POSTS, USER_BY_ACCOUNT_ID } from "graphql/quries";
+import {
+  GetPostsForm,
+  GetPostsResponse,
+  UserByAccountIdResponse,
+} from "graphql/quries.type";
 import { GetServerSidePropsContext, NextPage } from "next";
 import cookies from "next-cookies";
 import type { User } from "hooks/useUser";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
-
-const GET_POSTS = gql`
-  query GetPosts($count: Int!, $filter: filter) {
-    getPosts(getPostsData: { count: $count, filter: $filter }) {
-      posts {
-        _id
-        author {
-          _id
-          name
-        }
-        content
-        tags
-        category
-        createdAt
-      }
-      lastDateTime
-    }
-  }
-`;
-
-interface Filter {
-  userId?: string;
-  category?: string;
-  before?: string;
-}
-
-interface GetPostsForm {
-  count: number;
-  filter?: Filter;
-}
-
-interface GetPostsResponse {
-  getPosts: {
-    posts: {
-      _id: string;
-      author: User;
-      content: string;
-      tags: string;
-      category: string;
-      createdAt: string;
-    }[];
-    lastDateTime: string;
-  };
-}
+import { useQuery } from "@apollo/client";
 
 interface UserPageProps {
   profile: User | null;
@@ -70,7 +30,7 @@ const UserPage: NextPage<UserPageProps> = ({ isMe, profile }) => {
   const { data, loading, refetch } = useQuery<GetPostsResponse, GetPostsForm>(
     GET_POSTS,
     {
-      variables: { count: 20, filter: { userId: profile?._id } },
+      variables: { count: 20, targetUserId: profile?._id },
     }
   );
 
